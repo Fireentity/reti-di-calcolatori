19d222ca25c201b77995f37a984b4d3a887b5cc6
Fireentity <croceclaudio57@gmail.com>
2024-06-06 21:25:13 +0200

    feat: plots

--- analyzer.py
+++ analyzer.py
@@ -17,42 +17,39 @@ def main():
         data = {}
         for row in reader:
             server = row['server']
-            packet_size = float(row['payload_size'])
-            ping_result = row['avg_rtt']
 
             if server not in data:
                 data[server] = []
-            data[server].append({'avg_rtt': ping_result, 'packet_size': packet_size})
+            data[server].append(row)
 
         for server, data in data.items():
 
-            packet_size = [point['packet_size'] for point in data]
+            packet_size = [point['payload_size'] for point in data]
             avg_rtt = [point['avg_rtt'] for point in data]
             min_rtt = [point['min_rtt'] for point in data]
             max_rtt = [point['max_rtt'] for point in data]
-
+            plt.figure(figsize=(10, 6))
             plt.plot(packet_size, avg_rtt, color='red', marker='o', label=f'{server} - Average RTT')
             plt.plot(packet_size, min_rtt, color='blue', marker='o', label=f'{server} - Minimum RTT')
             plt.plot(packet_size, max_rtt, color='green', marker='o', label=f'{server} - Maximum RTT')
-
             plt.xlabel('Packet Size (bytes)')
             plt.ylabel('RTT (ms)')
             plt.title(f'RTT vs Packet Size - {server}')
+            plt.tight_layout()
             plt.legend()
+            plt.grid(True)
             plt.savefig(f'rtt_graph_{server}.png')
             plt.close()
 
-        for server, data in data.items():
-
-            packet_size = [point['packet_size'] for point in data]
             std_rtt = [point['std_rtt'] for point in data]
-
+            plt.figure(figsize=(10, 6))
             plt.plot(packet_size, std_rtt, marker='o', label=f'{server} - Standard deviation RTT')
-
             plt.xlabel('Packet Size (bytes)')
             plt.ylabel('RTT (ms)')
             plt.title(f'RTT vs Packet Size - {server}')
+            plt.tight_layout()
             plt.legend()
+            plt.grid(True)
             plt.savefig(f'rtt_std_graph_{server}.png')
             plt.close()
 
